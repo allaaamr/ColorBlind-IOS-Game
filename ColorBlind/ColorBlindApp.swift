8c314c822ae693f258b41183fdf2b0f5056fc651
Alaa Amr Mansour <alaa.amr1@hotmail.com>
2022-08-29 10:17:05 +0200

    Initial Commit

---
+++ ColorBlind/ColorBlindApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorBlindApp.swift
+//  ColorBlind
+//
+//  Created by Alaa Amr Abdelazeem on 29/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorBlindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
