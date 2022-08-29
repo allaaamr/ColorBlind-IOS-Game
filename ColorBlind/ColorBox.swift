ba32d60f69ae1ade0cb5851b46cb5523cbf7d3de
Alaa Amr Mansour <alaa.amr1@hotmail.com>
2022-08-29 13:22:18 +0200

    Init Commit

---
+++ ColorBlind/ColorBox.swift
@@ -0,0 +1,60 @@
+//
+//  ColorBox.swift
+//  ColorBlind
+//
+//  Created by Alaa Amr Abdelazeem on 29/08/2022.
+//
+
+import SwiftUI
+
+extension Color {
+    init?(hex: String) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+
+        var rgb: UInt64 = 0
+
+        var r: CGFloat = 0.0
+        var g: CGFloat = 0.0
+        var b: CGFloat = 0.0
+        var a: CGFloat = 1.0
+
+        let length = hexSanitized.count
+
+        guard Scanner(string: hexSanitized).scanHexInt64(&rgb) else { return nil }
+
+        if length == 6 {
+            r = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
+            g = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
+            b = CGFloat(rgb & 0x0000FF) / 255.0
+
+        } else if length == 8 {
+            r = CGFloat((rgb & 0xFF000000) >> 24) / 255.0
+            g = CGFloat((rgb & 0x00FF0000) >> 16) / 255.0
+            b = CGFloat((rgb & 0x0000FF00) >> 8) / 255.0
+            a = CGFloat(rgb & 0x000000FF) / 255.0
+
+        } else {
+            return nil
+        }
+
+        self.init(red: r, green: g, blue: b, opacity: a)
+    }
+}
+
+struct ColorBox: View {
+    var shade: String
+    var body: some View {
+        ZStack{
+            Rectangle()
+                .frame(width: 80, height: 50)
+                .foregroundColor(Color.init(hex: shade))
+        }
+    }
+}
+
+struct ColorBox_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorBox(shade: "f28500")
+    }
+}
--- ColorBlind/ContentView.swift
+++ ColorBlind/ContentView.swift
@@ -7,10 +7,100 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    @State private var GameTitle = "OrangeBlind"
+    @State private var gameEnded = false
+    @State private var shades =
+        ["#ffb300","#ffa812","#ffa700","#ff9f00",
+         "#ff8f00","#ff8c00","#ff7f00","#f77f00",
+         
+         "#ff7518", "#ffbf00", "#ffba00", "#f28500",
+         "#fb9902", "#ffa813", "#ffa811", "#ff9944"
+    ]
+    @State private var randomShadeIndex = 0
+    private var ranges = [(0..<4),(4..<8),(8..<12),(12..<16)]
+    @State private var attempts = 4
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Color.black
+                .ignoresSafeArea()
+            VStack {
+                Text(GameTitle)
+                    .foregroundColor(.orange)
+                    .font(.system(size: 30))
+                    .alert(GameTitle, isPresented: $gameEnded) {
+                        Button("Reset", role: .destructive, action: resetGame)
+                    }
+                Spacer()
+                Text("Can you get the color?")
+                    .foregroundColor(.white)
+                    .font(.system(size: 22))
+                ColorBox(shade: generateRandom())
+                Text("Remaining Attempts: \(attempts)")
+                    .foregroundColor(.white)
+                    .font(.system(size: 15))
+                Spacer()
+                ForEach(ranges, id: \.self) { range in
+                    HStack {
+                        ForEach(range, id: \.self) { i in
+                            ColorBox(shade: shades[i])
+                                .simultaneousGesture(
+                                    TapGesture()
+                                        .onEnded { _ in
+                                            print("Tap: \(i)")
+                                            playerTap(index: i)
+                                        }
+                                )
+                        }
+                    }
+                }
+                Spacer()
+                Button("Reset") {
+                    resetGame()
+                }
+            }
+            
+        }
+    }
+    
+    func generateRandom() -> String{
+        var x = Int.random(in: 0..<15)
+        print(x)
+        randomShadeIndex = x
+        return shades[x]
+        
+    }
+    
+    func resetGame() {
+        GameTitle = "OrangeBlind"
+        attempts = 4
+        shades = ["#fe5a1d","#ed872d","#ff7518","#fd5800",
+         "#ff7e00","#ff9933","#ff7f00","#ff8c00",
+         "#ff8f00", "#ffbf00", "#ffba00", "#f28500",
+                  "#cd5700", "#ec5800", "#e86100", "#f77f00"].shuffled()
+    }
+    func playerTap(index: Int){
+        if shades[index] != "#000000"{
+            if index != randomShadeIndex {
+                if attempts > 0 {
+                    attempts -= 1
+                    shades[index] = "#000000"
+                    
+                }
+                else{
+                    gameEnded = true
+                    GameTitle = "You Lost"
+                }
+            }
+            else {
+                gameEnded = true
+                GameTitle = "You Won"
+            }
+        }
     }
 }
 
